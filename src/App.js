4d11f9cb7a84d7a23e857f661572c282e8e2ada3
torn6810 <torn6810@gmail.com>
2021-11-27 21:40:13 -0800

    Added github/open source repository link

--- src/App.js
+++ src/App.js
@@ -5,6 +5,16 @@ function App() {
   return (
     <div className="App">
       Weather App
+      <footer> 
+      <a 
+      href="https://github.com/torn6810/react-weather-project" 
+      target="_blank"
+      rel="noreferrer"
+      >
+      Open-source code </a>
+
+      by Edith Tornel
+      </footer>
     </div>
   );
 }
